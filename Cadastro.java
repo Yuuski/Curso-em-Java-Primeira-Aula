27d22bca9df4269d1e65be96ce210c0c81f2866a
Ikkun <91970943+Yuuski@users.noreply.github.com>
2026-03-30 21:39:39 -0300

    Aula 2 Cadastro usuário
    Java

---
+++ Cadastro.java
@@ -0,0 +1,20 @@
+public class Cadastro {
+	public static void main(String[] args) {
+		byte idade = 28;
+		short ano = 2026;
+		int populacaoCidade = 500000;
+		long populacaoMundial = 8000000000L;
+		float altura = 1.79f;
+		double salario = 2700.50;
+		boolean estudante = true;
+		char inicialNome = 'A';
+
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano: " + ano);
+		System.out.println("População da cidade: " + populacaoCidade);
+		System.out.println("População Mundial: " + populacaoMundial);
+		System.out.println("Salário: " + salario);
+		System.out.println("É estudante? " + estudante);
+		System.out.println("Inicial do nome: " + inicialNome);
+	}
+}
